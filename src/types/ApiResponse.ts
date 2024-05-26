4845fb2f9f5b53710485dd71a129bfa5ae8980d0
unknown <priyanshu16181389@gmail.com>
2024-05-26 12:11:44 +0530

    completed signup

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,8 @@
+import { Message } from "@/model/user.models";
+
+export interface ApiResponse{
+    success: boolean;
+    message: string;
+    isAcceptingMessages?: boolean;
+    messages?: Array<Message>
+}
